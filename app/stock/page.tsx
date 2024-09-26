3333f5baf6380a630af6f6b283840d2c6b6743ff
진승현 <hynn@jinseunghyeon-ui-MacBookPro.local>
2024-09-26 18:37:23 +0900

    fix : request

--- app/stock/page.tsx
+++ app/stock/page.tsx
@@ -1,8 +1,9 @@
+'use client';
 import React, { useEffect, useState } from 'react';
 
 const Tables: React.FC = () => {
     const [token, setToken] = useState<string | null>(null);
-    const [topTradedStocks, setTopTradedStocks] = useState<any[]>([]);
+    const [topTradedStocks, setTopTradedStocks] = useState([]);
 
     const appKey = 'PSdHNByWrAtHQHJdgPni4klEcS2UhsjmyBqn';
     const appSecret = 'd4H+5BzaVvA4EQGe4gUAVfjQF9erxySxkfk8aCRW9TUTShnoKMOY6XqOOotCnD83MUx8RZL6s3h7qNfoOesFdoaW1IOt2e7hjcn24AD996UMFmu1mBE0UIMNxAFv4Ow3Om7MqI4s7x11zEi0fJTDfgPfEwYyc6E77+6Kphmny3F6S9kJaCY=';
@@ -27,6 +28,7 @@ const Tables: React.FC = () => {
             const data = await response.json();
             setToken(data.access_token);
             fetchTopStocks(data.access_token);
+            console.log(response);
         } catch (err) {
             console.error(`토큰을 불러오는 데 실패했습니다: ${err.message}`);
         }
@@ -41,7 +43,6 @@ const Tables: React.FC = () => {
             appkey: appKey,
             appsecret: appSecret,
         };
-
         const queryParams = new URLSearchParams({
             tr_id: 'FHPST01710000',
         });
@@ -50,6 +51,19 @@ const Tables: React.FC = () => {
             const response = await fetch(`${url}?${queryParams.toString()}`, {
                 method: 'GET',
                 headers: headers,
+                // body: JSON.stringify({
+                //     FID_COND_MRKT_DIV_CODE: 'J',
+                //     FID_COND_SCR_DIV_CODE: '20171',
+                //     FID_INPUT_ISCD: '0000',
+                //     FID_DIV_CLS_CODE: '0',
+                //     FID_BLNG_CLS_CODE: '0',
+                //     FID_TRGT_CLS_CODE: '111111111',
+                //     FID_TRGT_EXLS_CLS_CODE: '000000',
+                //     FID_INPUT_PRICE_1: '0',
+                //     FID_INPUT_PRICE_2: '0',
+                //     FID_VOL_CNT: '0',
+                //     FID_INPUT_DATE_1: '0',
+                // }),
             });
 
             const data = await response.json();
