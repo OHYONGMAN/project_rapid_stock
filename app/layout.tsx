2f14cbc8d52442df03954159c18e6bd8fb25b171
진승현 <hynn@jinseunghyeon-ui-MacBookPro.local>
2024-09-27 10:55:32 +0900

    fix : useToken 기능

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css';
-import Header from '../components/Header';
+import Header from './components/Header';
 import Head from 'next/head';
 
 export default function RootLayout({
--- app/stock/page.tsx
+++ app/stock/page.tsx
@@ -1,7 +1,21 @@
+'use client';
+import useToken from '../../hook/useToken';
+
 export default function Home() {
+    const { token, loading, error } = useToken();
+
+    // function fetchStock() {
+    //     const url = '';
+    // }
+
     return (
         <div>
             <h1>stock</h1>
+            <p>
+                {token}
+                {error}
+                {loading}
+            </p>
         </div>
     );
 }
