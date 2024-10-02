64ca6706c1444b80b81cc40cb46cdd700dbbf9ac
jang.yuju <ujoo2802@gmail.com>
2024-10-02 11:22:24 +0900

    초기 세팅

--- project_rapid_stock/app/news/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-    return (
-        <div>
-            <h1>News</h1>
-        </div>
-    );
-}
--- project_rapid_stock/app/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return (
-    <h1 className="bg-slate-300">안녕하세요</h1>
-  )
-}
