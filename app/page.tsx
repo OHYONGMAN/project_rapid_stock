cbba6de8f70a4bcfe8944a7073e18f2b41c575b4
진승현 <hynn@jinseunghyeon-ui-MacBookPro.local>
2024-10-02 15:11:05 +0900

    feat : RankStock 기능 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import StockRank from './components/StockRank/StockRank';
+
 export default function Home() {
-  return <h1 className="bg-slate-300">안녕하세요</h1>;
+  return (
+    <>
+      <StockRank />
+    </>
+  );
 }
