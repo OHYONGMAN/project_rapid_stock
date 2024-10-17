520ba6532aaa3d2bc792cce3bb02e17c07dd16c9
OHYONGMAN <oymin1998@naver.com>
2024-10-17 17:18:45 +0900

    docs: 에러 수정

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, Suspense } from 'react';
 import { supabase } from '../utils/supabase.ts';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -28,6 +28,7 @@ const News: React.FC = () => {
   const itemsPerPage = 10;
   const [hasMore, setHasMore] = useState<boolean>(true);
 
+  // Suspense를 사용하여 useSearchParams를 감싸기
   const searchParams = useSearchParams();
   const searchTerm = searchParams.get('search') || undefined;
 
@@ -129,50 +130,52 @@ const News: React.FC = () => {
   };
 
   return (
-    <ul className="m-auto w-[1282px] p-5">
-      {filteredNewsData.map((news) => (
-        <li className="mt-10 flex flex-col" key={news.id}>
-          <div className="flex justify-between">
-            <div className="flex">
-              <Image
-                src={news.image}
-                alt="뉴스 이미지"
-                width={100}
-                height={50}
-                objectFit="cover"
-              />
-              <Link href={`/news/${news.id}`}>
-                <h2
-                  className="ml-4 cursor-pointer text-xl font-bold"
-                  onClick={() => handleNewsClick(news.id, news.title)}
-                >
-                  {news.title}
-                </h2>
-              </Link>
+    <Suspense fallback={<div>Loading...</div>}>
+      <ul className="m-auto w-[1282px] p-5">
+        {filteredNewsData.map((news) => (
+          <li className="mt-10 flex flex-col" key={news.id}>
+            <div className="flex justify-between">
+              <div className="flex">
+                <Image
+                  src={news.image}
+                  alt="뉴스 이미지"
+                  width={100}
+                  height={50}
+                  objectFit="cover"
+                />
+                <Link href={`/news/${news.id}`}>
+                  <h2
+                    className="ml-4 cursor-pointer text-xl font-bold"
+                    onClick={() => handleNewsClick(news.id, news.title)}
+                  >
+                    {news.title}
+                  </h2>
+                </Link>
+              </div>
+              <div>
+                <p>{news.date}</p>
+              </div>
             </div>
-            <div>
-              <p>{news.date}</p>
-            </div>
-          </div>
-          <p className="mt-3 font-semibold">
-            <span className="m-3">관련종목</span>
-            {news.keyword.map((keyword, index) => (
-              <span key={index} className="mr-2">
-                {keyword}
-              </span>
-            ))}
-          </p>
-        </li>
-      ))}
+            <p className="mt-3 font-semibold">
+              <span className="m-3">관련종목</span>
+              {news.keyword.map((keyword, index) => (
+                <span key={index} className="mr-2">
+                  {keyword}
+                </span>
+              ))}
+            </p>
+          </li>
+        ))}
 
-      {loading && <p>로딩중</p>}
+        {loading && <p>로딩중</p>}
 
-      {!loading && hasMore && (
-        <div ref={observerRef} style={{ height: '20px' }}></div>
-      )}
+        {!loading && hasMore && (
+          <div ref={observerRef} style={{ height: '20px' }}></div>
+        )}
 
-      {!hasMore && <p>데이터가 없습니다.</p>}
-    </ul>
+        {!hasMore && <p>데이터가 없습니다.</p>}
+      </ul>
+    </Suspense>
   );
 };
 
