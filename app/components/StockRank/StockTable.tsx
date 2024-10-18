8262fc804e8caf9d37cd76a06e7a6984aa0b469e
OHYONGMAN <oymin1998@naver.com>
2024-10-18 10:56:55 +0900

    다시 한번

--- app/components/StockRank/StockTable.tsx
+++ app/components/StockRank/StockTable.tsx
@@ -4,7 +4,12 @@ import React, { useState } from 'react';
 import StockRank from './StockRank.tsx';
 import StockCapitalization from './StockCapitalization.tsx';
 
-const tabs = [
+type Tab = {
+  name: string;
+  component: JSX.Element;
+};
+
+const tabs: Tab[] = [
   { name: '거래 상위', component: <StockRank /> },
   { name: '시가총액 상위', component: <StockCapitalization /> },
 ];
@@ -14,7 +19,12 @@ export default function StockTable({
 }: {
   searchParams: { tab?: string };
 }) {
-  const [activeTab, setActiveTab] = useState(parseInt(searchParams.tab || '0'));
+  // searchParams를 사용해 초기 탭을 설정하거나 기본값으로 0을 사용합니다.
+  const [activeTab, setActiveTab] = useState(
+    searchParams.tab ? parseInt(searchParams.tab) : 0,
+  );
+
+  const ActiveComponent = tabs[activeTab]?.component || tabs[0].component;
 
   return (
     <div className="mx-auto">
@@ -34,7 +44,7 @@ export default function StockTable({
         ))}
       </div>
 
-      <div className="tab-content mt-4">{tabs[activeTab]?.component}</div>
+      <div className="tab-content mt-4">{ActiveComponent}</div>
     </div>
   );
 }
